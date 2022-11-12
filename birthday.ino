8e656781eb1e5403ca6600637200cc7638bf1ac9
zhangyanhua <zhangyanhua@huawei.com>
2022-11-12 12:34:01 +0800

    解决问题

--- birthday.ino
+++ birthday.ino
@@ -7,9 +7,6 @@
 volatile int ClockRSTPin;
 volatile int ClockDATPin;
 volatile int ClockClkPin;
-String _LsTime;
-String _LsDate;
-volatile unsigned long _LsSec;
 volatile int setYear;
 volatile int setmonth;
 volatile int setday;
@@ -18,7 +15,6 @@ volatile int setminute;
 volatile int secondChangeFlag;
 volatile int setsecond;
 volatile int Lastsetsecond;
-volatile unsigned long TotalSecond;
 volatile int currentSecond;
 volatile int setweek;
 volatile int currminisecond;
@@ -40,20 +36,14 @@ volatile int setPin;
 volatile int ledPin;
 volatile int execFlag;
 volatile int NotOpSecond;
-String tmp;
-String tmp2;
-String tmp3;
-volatile int tmp4;
+String tmp_string;
+volatile int tmp_int;
 String weekstring[] = {"", "Mon.", "Tue.", "Wed.", "Thu.", "Fri.", "Sat.", "Sun."};
 char *monthstring[] = {"", "Jan", "Feb", "Mar", "Apr", "May", "Jun", "July", "Agu", "Sep", "Oct", "Nov", "Dec"};
-
 int maxdays[] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-
 LiquidCrystal_I2C mylcd(0x27, 16, 2);
 String birthText1;
 String birthText2;
-volatile int temp;
-volatile int humi;
 String temperaturetext;
 String humiditytext;
 volatile int tempdisplaySec;
@@ -67,24 +57,33 @@ DHT dhtA2(A2, 11);
 
 void clock_init()
 {
-    Rtc.SetDateTime(RtcDateTime(__DATE__, __TIME__));
+    //Rtc.SetDateTime(RtcDateTime(__DATE__, __TIME__));
     get_current_time();
 }
 
 void LED_init()
 {
-    // digitalWrite(9,HIGH);
     mylcd.backlight();
 }
 
 void Music_init()
 {
-    delay(10);
     pinMode(musicSetPin, OUTPUT);
     digitalWrite(musicSetPin, LOW);
     pinMode(ledPin, OUTPUT);
     digitalWrite(ledPin, LOW);
     play_music(3);
+}
+
+void get_current_time()
+{
+    setYear = Rtc.GetDateTime().Year();
+    setmonth = Rtc.GetDateTime().Month();
+    setday = Rtc.GetDateTime().Day();
+    sethour = Rtc.GetDateTime().Hour();
+    setminute = Rtc.GetDateTime().Minute();
+    setsecond = Rtc.GetDateTime().Second();
+    setweek = Rtc.GetDateTime().DayOfWeek();
 }
 
 void main_process()
@@ -105,169 +104,11 @@ void main_process()
     ledbacklight_proc();
 }
 
-void ledbacklight_proc()
-{
-    if (secondChangeFlag == 1)
-    {
-        // 超过10秒不操作，关闭背光
-        if (NotOpSecond < 10)
-        {
-            mylcd.backlight();
-        }
-        else
-        {
-            mylcd.noBacklight();
-        }
-    }
-}
-
-void setproc_display()
-{
-    currminisecond = millis();
-    if (currminisecond - lastminisecond > 250)
-    {
-        lastminisecond = currminisecond;
-        blink = (long)((blink + 1)) % (long)(2);
-        pinMode(ledPin, OUTPUT);
-        digitalWrite(ledPin, LOW);
-
-        switch (setFlag)
-        {
-        case 0:
-            break;
-        case 1:
-            display_num_blink(2, 2, setYear);
-            break;
-        case 2:
-            display_num_noblink(2, 2, setYear);
-            display_num_blink(2, 7, setmonth);
-            break;
-        case 3:
-            display_num_noblink(2, 7, setmonth);
-            display_num_blink(2, 10, setday);
-            setdoneFlag = 1;
-            break;
-        case 4:
-
-            display_num_noblink(2, 10, setday);
-            if (setdoneFlag == 1)
-            {
-                // Serial.print(String("set: ") + String(setYear)+ String("-") + String(setmonth) + String("-") + String(setday) );
-                // Serial.println(String(" ") + String(sethour)+ String(":") + String(setminute) + String(":") + String(setsecond));
-                set_time_to_rtc(setYear, setmonth, setday, sethour, setminute, setsecond, setweek);
-                mylcd.setCursor(0, 1);
-                mylcd.print("Date Set OK");
-                delay(1000);
-                setFlag = 5;
-            }
-
-            delay(10);
-            display_current_time3();
-            // Serial.print(String("after: ") + String(setYear)+ String("-") + String(setmonth) + String("-") + String(setday) );
-            // Serial.println(String(" ") + String(sethour)+ String(":") + String(setminute) + String(":") + String(setsecond));
-
-            setdoneFlag = 0;
-            break;
-        case 5:
-            display_num_blink(1, 5, sethour);
-            break;
-        case 6:
-            display_num_noblink(1, 5, sethour);
-            display_num_blink(1, 8, setminute);
-            break;
-        case 7:
-            display_num_noblink(1, 8, setminute);
-            display_num_blink(1, 11, setsecond);
-            setdoneFlag = 1;
-            break;
-        case 8:
-            if (setdoneFlag == 1)
-            {
-                display_num_noblink(1, 11, setsecond);
-                set_time_to_rtc(setYear, setmonth, setday, sethour, setminute, setsecond, setweek);
-                delay(10);
-                mylcd.setCursor(0, 0);
-                mylcd.print("Time Set OK");
-                delay(1000);
-                setFlag = 0;
-            }
-            display_current_time3();
-
-            setdoneFlag = 0;
-            break;
-        default:
-            break;
-        }
-    }
-}
-
-void display_num_blink(int x, int y, int z)
-{
-    if (z > 1000)
-    { // year 与4个空格
-        tmp3 = "    ";
-    }
-    else
-    {
-        tmp3 = "  "; //其他的都是2位
-    }
-    if (blink == 1)
-    {
-        tmp = adjust_string(z);
-    }
-    else
-    {
-        tmp = tmp3;
-    }
-    mylcd.setCursor(y - 1, x - 1);
-    mylcd.print(tmp);
-}
-
-void display_num_noblink(int x, int y, int z)
-{
-    tmp = adjust_string(z);
-    mylcd.setCursor(y - 1, x - 1);
-    mylcd.print(tmp);
-}
-boolean mixly_digitalRead(uint8_t pin)
-{
-    pinMode(pin, INPUT);
-    boolean _return = digitalRead(pin);
-    pinMode(pin, OUTPUT);
-    return _return;
-}
-
-String adjust_string(int x)
-{
-    tmp2 = "";
-    if (x >= 0 && x <= 9)
-    {
-        tmp2 = String("0") + String(x);
-    }
-    else
-    {
-        tmp2 = x;
-    }
-    return tmp2;
-}
-
-void get_current_time()
-{
-    setYear = Rtc.GetDateTime().Year();
-    setmonth = Rtc.GetDateTime().Month();
-    setday = Rtc.GetDateTime().Day();
-    sethour = Rtc.GetDateTime().Hour();
-    setminute = Rtc.GetDateTime().Minute();
-    setsecond = Rtc.GetDateTime().Second();
-    setweek = Rtc.GetDateTime().DayOfWeek();
-}
-
 void refresh_second()
 {
     currentSecond = Rtc.GetDateTime().Second();
     if (currentSecond != Lastsetsecond)
     {
-        TotalSecond = TotalSecond + 1;
         Lastsetsecond = currentSecond;
         secondChangeFlag = 1;
         NotOpSecond = NotOpSecond + 1;
@@ -295,8 +136,6 @@ void refresh_set_key()
             // 6、设置状态，设置分
             // 7、设置状态，设置秒
             // 8、时间生效
-            //
-
             setFlag = (long)((setFlag + 1)) % (long)(9);
             if (setFlag == 0)
             {
@@ -306,10 +145,8 @@ void refresh_set_key()
             }
             if (setFlag == 1 || setFlag == 5)
             {
-
-                mylcd.clear();
                 play_music(3);
-                display_current_time3();
+                display_current_time();
             }
         }
         else
@@ -344,54 +181,196 @@ void refresh_adjust_key()
     }
     LastAdjustInput = AdjustInput;
 }
+void LedLight(int status)
+{
+    pinMode(ledPin, OUTPUT);
+    digitalWrite(ledPin, status);
+}
 
 void main_display()
 {
     if (execFlag == 1)
     {
         execFlag = 0;
-        mylcd.clear();
         switch (menuValue)
         {
         case 0:
-            pinMode(ledPin, OUTPUT);
-            digitalWrite(ledPin, LOW);
+            LedLight(LOW);
             play_music(3);
-            display_current_time3();
+            display_current_time();
             break;
         case 1:
-            pinMode(ledPin, OUTPUT);
-            digitalWrite(ledPin, LOW);
+            LedLight(LOW);
             play_music(3);
-            display_temperature();
+            display_temperature(1);
             break;
         case 2:
-            pinMode(ledPin, OUTPUT);
-            digitalWrite(ledPin, HIGH);
+            LedLight(HIGH);
             display_birthday_string();
             play_music(1);
             break;
         case 3:
-            pinMode(ledPin, OUTPUT);
-            digitalWrite(ledPin, HIGH);
+            LedLight(HIGH);
             display_birthday_string();
             play_music(2);
             break;
         default:
-            pinMode(ledPin, OUTPUT);
-            digitalWrite(ledPin, LOW);
+            LedLight(LOW);
             break;
         }
     }
     switch (menuValue)
     {
     case 0:
-        display_current_time();
+        if (secondChangeFlag == 1)
+        {
+            display_current_time();
+        }
         break;
     case 1:
-        display_temperature();
+        display_temperature(0);
         break;
     }
+}
+
+void ledbacklight_proc()
+{
+    if (secondChangeFlag == 1)
+    {
+        // 超过10秒不操作，关闭背光
+        if (NotOpSecond < 10)
+        {
+            mylcd.backlight();
+        }
+        else
+        {
+            mylcd.noBacklight();
+            if (NotOpSecond > 600)
+            {
+                LedLight(LOW);
+                play_music(3);
+            }
+        }
+    }
+}
+
+void setproc_display()
+{
+    currminisecond = millis();
+    if (currminisecond - lastminisecond > 250)
+    {
+        lastminisecond = currminisecond;
+        blink = (long)((blink + 1)) % (long)(2);
+        LedLight(LOW);
+
+        switch (setFlag)
+        {
+        case 0:
+            break;
+        case 1:
+            display_num_blink(2, 2, setYear);
+            break;
+        case 2:
+            display_num_noblink(2, 2, setYear);
+            display_num_blink(2, 7, setmonth);
+            break;
+        case 3:
+            display_num_noblink(2, 7, setmonth);
+            display_num_blink(2, 10, setday);
+            setdoneFlag = 1;
+            break;
+        case 4:
+            display_num_noblink(2, 10, setday);
+            if (setdoneFlag == 1)
+            {
+                // Serial.print(String("set: ") + String(setYear)+ String("-") + String(setmonth) + String("-") + String(setday) );
+                // Serial.println(String(" ") + String(sethour)+ String(":") + String(setminute) + String(":") + String(setsecond));
+                set_time_to_rtc(setYear, setmonth, setday, sethour, setminute, setsecond, setweek);
+                mylcd.setCursor(0, 1);
+                mylcd.print("   Date Set OK  ");
+                delay(1500);
+                setFlag = 5;
+            }
+            display_current_time();
+            setdoneFlag = 0;
+            break;
+        case 5:
+            display_num_blink(1, 5, sethour);
+            break;
+        case 6:
+            display_num_noblink(1, 5, sethour);
+            display_num_blink(1, 8, setminute);
+            break;
+        case 7:
+            display_num_noblink(1, 8, setminute);
+            display_num_blink(1, 11, setsecond);
+            setdoneFlag = 1;
+            break;
+        case 8:
+            if (setdoneFlag == 1)
+            {
+                display_num_noblink(1, 11, setsecond);
+                set_time_to_rtc(setYear, setmonth, setday, sethour, setminute, setsecond, setweek);
+                delay(10);
+                mylcd.setCursor(0, 0);
+                mylcd.print("   Time Set OK");
+                delay(1500);
+                setFlag = 0;
+            }
+            display_current_time();
+
+            setdoneFlag = 0;
+            break;
+        default:
+            break;
+        }
+    }
+}
+String adjust_string(int x)
+{
+    tmp_string = "";
+    if (x >= 0 && x <= 9)
+    {
+        tmp_string = String("0") + String(x);
+    }
+    else
+    {
+        tmp_string = x;
+    }
+    return tmp_string;
+}
+
+void display_num_blink(int x, int y, int z)
+{
+    if (blink == 1)
+    {
+        tmp_string = adjust_string(z);
+    }
+    else
+    { // year 与4个空格//其他的都是2位
+        tmp_string = "  ";
+        if (z > 1000)
+        {
+            tmp_string = "    ";
+        }
+    }
+    mylcd.setCursor(y - 1, x - 1);
+    mylcd.print(tmp_string);
+}
+
+void display_num_noblink(int x, int y, int z)
+{
+    tmp_string = adjust_string(z);
+    mylcd.setCursor(y - 1, x - 1);
+    mylcd.print(tmp_string);
+}
+
+boolean mixly_digitalRead(uint8_t pin)
+{
+    pinMode(pin, INPUT);
+    boolean _return = digitalRead(pin);
+    pinMode(pin, OUTPUT);
+    return _return;
 }
 
 void refresh_set_time_num()
@@ -416,12 +395,12 @@ void refresh_set_time_num()
             break;
         case 3:
             setday = setday + 1;
-            tmp4 = maxdays[setmonth];
+            tmp_int = maxdays[setmonth];
             if ((long)(setYear) % (long)(4) == 0)
             {
-                tmp4 = 29;
+                tmp_int = 29;
             }
-            if (setday > tmp4)
+            if (setday > tmp_int)
             {
                 setday = 1;
             }
@@ -457,6 +436,14 @@ void refresh_main_list()
     menuValue = (long)((menuValue + 1)) % (long)(4);
     execFlag = 1;
 }
+void music_pin_Trig(int pin_num)
+{
+    pinMode(pin_num, OUTPUT);
+    digitalWrite(pin_num, LOW);
+    delay(30);
+    pinMode(pin_num, OUTPUT);
+    digitalWrite(pin_num, HIGH);
+}
 
 void play_music(int x)
 {
@@ -465,36 +452,26 @@ void play_music(int x)
     switch (x)
     {
     case 1:
-        pinMode(music1Pin, OUTPUT);
-        digitalWrite(music1Pin, LOW);
-        delay(30);
-        pinMode(music1Pin, OUTPUT);
-        digitalWrite(music1Pin, HIGH);
-
+        music_pin_Trig(music1Pin);
         break;
     case 2:
-        pinMode(music2Pin, OUTPUT);
-        digitalWrite(music2Pin, LOW);
-        delay(30);
-        pinMode(music2Pin, OUTPUT);
-        digitalWrite(music2Pin, HIGH);
+        music_pin_Trig(music2Pin);
         break;
     case 3:
-        pinMode(music3Pin, OUTPUT);
-        digitalWrite(music3Pin, LOW);
-        delay(30);
-        pinMode(music3Pin, OUTPUT);
-        digitalWrite(music3Pin, HIGH);
+        music_pin_Trig(music3Pin);
         break;
     }
 }
 
-void display_temperature()
+void display_temperature(int clearFlag)
 {
     if (secondChangeFlag == 1)
     {
-        temp = dhtA2.readTemperature();
-        humi = dhtA2.readHumidity();
+        int temp = dhtA2.readTemperature();
+        int humi = dhtA2.readHumidity();  
+
+        //要先获取温度再清，避免获取温度时间太长导致白屏
+        mylcd.clear();      
         mylcd.setCursor(0, 0);
         mylcd.print(String(temperaturetext) + String(temp));
         mylcd.setCursor(0, 1);
@@ -517,25 +494,7 @@ void display_birthday_string()
 
 void display_current_time()
 {
-    if (secondChangeFlag == 1)
-    {
-        mylcd.clear();
-        display_current_time3();
-    }
-}
-
-void set_time_to_rtc(int year, int month, int day, int hour, int minute, int second, int week)
-{
-    char dateTmp[13];
-    char timeTmp[10];
-    sprintf(dateTmp, "%s/%02d/%02d", monthstring[month], day, year);
-    sprintf(timeTmp, "%02d:%02d:%02d", hour, minute, second);
-    RtcDateTime compiled = RtcDateTime(dateTmp, timeTmp);
-    Rtc.SetDateTime(compiled);
-}
-
-void display_current_time3()
-{
+    mylcd.clear();
     get_current_time();
     display_set_time();
     display_set_date();
@@ -543,6 +502,7 @@ void display_current_time3()
 
 void display_set_date()
 {
+    String _LsDate;
     _LsDate = String(setYear) + String("-");
     _LsDate = String(_LsDate) + String((adjust_string(setmonth)));
     _LsDate = String(_LsDate) + String("-");
@@ -555,6 +515,7 @@ void display_set_date()
 
 void display_set_time()
 {
+    String _LsTime;    
     _LsTime = String((adjust_string(sethour))) + String(":");
     _LsTime = String(_LsTime) + String((adjust_string(setminute)));
     _LsTime = String(_LsTime) + String(":");
@@ -564,14 +525,21 @@ void display_set_time()
     mylcd.print(_LsTime);
 }
 
+void set_time_to_rtc(int year, int month, int day, int hour, int minute, int second, int week)
+{
+    char dateTmp[13];
+    char timeTmp[10];
+    sprintf(dateTmp, "%s/%02d/%02d", monthstring[month], day, year);
+    sprintf(timeTmp, "%02d:%02d:%02d", hour, minute, second);
+    RtcDateTime compiled = RtcDateTime(dateTmp, timeTmp);
+    Rtc.SetDateTime(compiled);
+}
+
 void setup()
 {
     ClockRSTPin = 12;
     ClockDATPin = 11;
     ClockClkPin = 10;
-    _LsTime = "";
-    _LsDate = "";
-    _LsSec = 0;
     setYear = 0;
     setmonth = 0;
     setday = 0;
@@ -580,7 +548,6 @@ void setup()
     secondChangeFlag = 0;
     setsecond = 0;
     Lastsetsecond = 0;
-    TotalSecond = 0;
     currentSecond = 0;
     setweek = 0;
     currminisecond = 0;
@@ -602,10 +569,8 @@ void setup()
     ledPin = 7;
     execFlag = 1;
     NotOpSecond = 0;
-    tmp = "0";
-    tmp2 = "";
-    tmp3 = "";
-    tmp4 = "";
+    tmp_string = "";
+    tmp_int = "";
     LED_init();
     Music_init();
     clock_init();
@@ -616,8 +581,6 @@ void setup()
     birthText1 = ' ';
     birthText2 = ' ';
     // Serial.begin(9600);
-    temp = 0;
-    humi = 0;
     temperaturetext = "temperature: ";
     humiditytext = "humidity:    ";
     tempdisplaySec = 0;
